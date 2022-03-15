a7b0bf26be27ebd3b926d2abeaaaa4d4744ae352
Sreeram Padmanabhan <sreeram.padmanabhan@team.bumble.com>
2022-03-15 21:31:55 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Web Developer Interview Exercise</h1>
+      </header>
+      {/* Your code starts here */}
+    </div>
+  );
+}
+
+export default App;
